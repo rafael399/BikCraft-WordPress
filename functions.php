ec8beec8fd5cec64944ec773d5af20a996064077
rafael399 <rafael399@gmail.com>
2020-04-10 13:06:35 -0300

    Scripts e CSS

--- functions.php
+++ functions.php
@@ -1,5 +1,42 @@
 <?php 
 
+	// Função para registrar os Scripts
+	function register_scripts() {
+		// Desregistrar jQuery padrão do WordPress
+		wp_deregister_script('jquery');
+
+		// Registrar o novo jQuery
+		wp_register_script('jquery', get_template_directory_uri() . '/js/libs/jquery-1.11.2.min.js',
+			array(), '1.11.2', true);
+
+		// Registrar Plugins
+		wp_register_script('plugins-script', get_template_directory_uri() . '/js/plugins.js',
+			array('jquery'), false, true);
+
+		// Registrar Main
+		wp_register_script('main-script', get_template_directory_uri() . '/js/main.js',
+			array('jquery', 'plugins-script'), false, true);
+
+		// Registrar Modernizr
+		wp_register_script('modernizr', get_template_directory_uri() . '/js/libs/modernizr.custom.45655.js',
+			array(), '45655', false);
+
+		// Carregar scripts no site
+		wp_enqueue_script('modernizr');
+		wp_enqueue_script('main-script');
+	}
+	add_action('wp_enqueue_scripts', 'register_scripts');
+
+	// Função para registrar CSS
+	function register_css() {
+		// Registrar style.css
+		wp_register_style('main-style', get_template_directory_uri() . '/style.css', array(), false, false);
+
+		// Carregar CSS no site
+		wp_enqueue_style('main-style');
+	}
+	add_action('wp_enqueue_scripts', 'register_css');
+
 	// Funções para Limpar o Header
 	remove_action('wp_head', 'rsd_link');
 	remove_action('wp_head', 'wlwmanifest_link');
--- header.php
+++ header.php
@@ -14,9 +14,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<link rel="shortcut icon" href="favicon.ico">
-
-		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-    <script src="js/libs/modernizr.custom.45655.js"></script>
     
     <!-- Inicio Wordpress Header -->
     <?php wp_head(); ?>
